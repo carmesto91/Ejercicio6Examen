3fa1ac20b0d5798503f31f8e19634b11ac5f938f
Alumno <Alumno@913-m14.gabinete.frc.utn.edu.ar>
2018-09-28 20:38:43 -0300

    sql

--- 111milExamen6/src/main/resources/01-schema.sql
+++ 111milExamen6/src/main/resources/01-schema.sql
@@ -1,10 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Alumno
- * Created: 28-sep-2018
- */
+create database controlstock;
+use controlstock;
+
+
+create table computadora(
+id_computadora int,
+nombre varchar (45),
+modelo varchar (45),
+precio int,
+constraint id_pc_pk primary key(id_computadora)
+);
+
+create table ItemStock(
+Id_stock int,
+Id_computadora int,
+Cantidad int,
+constraint stock_pk primary key (id_stock),
+constraint id_pc_fk foreign key (Id_computadora) references computadora(id_computadora)
+);
+
 
