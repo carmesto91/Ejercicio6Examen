3643d51b4025d57247fa385d3a828746ca5683f1
alumno <alumno@913-m12.gabinete.frc.utn.edu.ar>
2018-10-03 19:58:55 -0300

    consulta

--- 111milExamen6/src/main/resources/01-schema.sql
+++ 111milExamen6/src/main/resources/01-schema.sql
@@ -18,4 +18,11 @@ constraint stock_pk primary key (id_stock),
 constraint id_pc_fk foreign key (Id_computadora) references computadora(id_computadora)
 );
 
+insert into computadora values (1, 'samsung', 'vcp', 20000 );
+insert into computadora values (2, 'acer', 'aspire 3',25000 );
+insert into computadora values (3, 'acer', 'aspire 5', 30000);
+insert into computadora values (4,'asus', 'rog', 40000);
+insert into computadora values (5, 'asus', 'vivobook', 24000);
+insert into computadora values (6,'hp','omen', 45000);
+insert into computadora values (7,'hp', 'pavilion',35000);
 
